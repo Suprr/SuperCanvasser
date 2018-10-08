c6a58e294f3a01bad6784593bee0fde5102acd6f
Suprr <michaelrizzo1996@gmail.com>
2018-10-08 15:45:38 -0400

    New Project Init

---
+++ src/main/java/team830/demo/SuperCanvasserApplication.java
@@ -0,0 +1,12 @@
+package team830.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCanvasserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCanvasserApplication.class, args);
+    }
+}
