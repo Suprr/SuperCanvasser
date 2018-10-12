e924099db6588e6edb6595b804375a0bd96913a6
chowg1762 <chwog1762@gmail.com>
2018-10-12 16:06:46 -0400

    src/App

--- front/src/App.js
+++ front/src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
     }else{
       page = <SignIn user = {this.state.user} signedIn={this.signInHandler}/>
     }
-
+    
     return (
       <BrowserRouter>
         <div>{page}</div>
