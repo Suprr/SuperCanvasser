32ef12b6a43119f887a25b7e2c413c06ded05696
Kat Choi <kat.ah.choi@gmail.com>
2018-10-13 16:46:54 -0400

    Revert "disaster for log in... not done at all"
    This reverts commit 2c8cbd13a1485a38da27dc37d80e7e6ca2c2370b.

--- src/main/java/team830/SuperCanvasser/DataAccessLayer/UserRepo.java
+++ src/main/java/team830/SuperCanvasser/DataAccessLayer/UserRepo.java
@@ -1,8 +1,10 @@
-package team830.SuperCanvasser.User;
+package team830.SuperCanvasser.DataAccessLayer;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import team830.SuperCanvasser.User.User;
 
 public interface UserRepo extends MongoRepository<User, ObjectId> {
-    User findByEmail(String email);
+    User findByUsername(String username);
+
 }
--- src/main/java/team830/SuperCanvasser/User/User.java
+++ src/main/java/team830/SuperCanvasser/User/User.java
@@ -1,72 +1,18 @@
 package team830.SuperCanvasser.User;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.validation.constraints.NotEmpty;
-import java.util.Collection;
-
-@Builder
 @Document(collection = "users")
-public class User implements UserDetails {
+public abstract class User {
     @Id
     private ObjectId id;
-
-    @NotEmpty(message = "Please type first name")
+    private String passHash;
     private String firstName;
-
-    @NotEmpty(message = "Please type last name")
     private String lastName;
     private String zipCode;
-    @NotEmpty(message = "Please type email")
     private String email;
-    private String pwdHash;
-
-    @NotEmpty(message = "Please select role(s)")
-    @DBRef
-    private Role[] roles;
-
-    @Override
-    public String getPassword() {
-        return pwdHash;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
+    private String[] role;
 
 }
