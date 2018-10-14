946f4bbf3214622ec24202eab00dcd26f571f8c0
Suprr <michaelrizzo1996@gmail.com>
2018-10-13 20:13:11 -0400

    Variable Controller Fully Operational

--- src/main/java/team830/SuperCanvasser/Variable/VariableController.java
+++ src/main/java/team830/SuperCanvasser/Variable/VariableController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 import java.util.List;
 @RequestMapping("/var")
@@ -14,10 +15,10 @@ public class VariableController {
     private VariableService variableService;
 
     @RequestMapping(value = "/edit/", method = RequestMethod.POST)
-    public Variable editVar(@ModelAttribute("var") Variable var,
-                            BindingResult result) {
-//        System.out.println(var.getValue());
+    public Variable editVar(@RequestBody Variable var, BindingResult result) {
+        System.out.println("####" + var);
         if (result.hasErrors()) {
+            System.out.println("FUCK");
             return null;
         } else {
             return (variableService.editVariable(var));
