63f2daa4327f8b5858c4a9cb137f08142ffe4395
EunhoKim219 <eunho.kim@stonybrook.edu>
2018-11-28 22:17:43 -0500

    same

--- front/src/axios.js
+++ front/src/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   //baseURL: "https://cse308-de3df.firebaseio.com"
-  baseURL : "http://localhost:3000"
-  
+  baseURL: "http://108.54.182.203:3000"
 });
 
 export default instance;
